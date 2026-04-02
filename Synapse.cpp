03206fd0d01834aee5d1393582337c531dea1113
nonyponymlg-png <nonyponymlg@gmail.com>
2026-04-02 08:56:07 -0500

    Refactor Synapse class and add weight methods
    Refactor Synapse class for better readability and add weight getter/setter methods.

--- Synapse.cpp
+++ Synapse.cpp
@@ -1,12 +1,26 @@
 class Synapse {
-    Neuron* n1, n2;
+    Neuron* n1;
+    Neuron* n2;
     float weight = 0;
+    
+public:
     Synapse(Neuron* _n1, Neuron* _n2, float _weight) {
-        n1=_n1;
-        n2=_n2;
-        weight=_weight;
+        n1 = _n1;
+        n2 = _n2;
+        weight = _weight;
     }
-    update() {
-        
+    
+    void update() {
+        if (n1->spike) {
+            n2->v += weight * n1->spike;
+        }
     }
-}
+    
+    void setWeight(float newWeight) {
+        weight = newWeight;
+    }
+    
+    float getWeight() {
+        return weight;
+    }
+};
