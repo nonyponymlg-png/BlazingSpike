1c55318376960344d7555a762e999d607cccc67e
nonyponymlg-png <nonyponymlg@gmail.com>
2026-04-02 09:00:02 -0500

    Create Brain class with basic structure

---
+++ Brain.cpp
@@ -0,0 +1,16 @@
+#include "Neuron.cpp"
+#include "Synapse.cpp"
+
+class Brain {
+private:
+  int numNeurons;
+  //initialize neuron+synapse arrays
+public:
+  Brain(_numNeurons) {
+    numNeurons = _numNeurons;
+    // Add neurons and synapses
+  }
+  void update() {
+    //update neurons
+  }
+}
